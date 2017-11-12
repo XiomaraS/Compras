1b17de34ff126c5c989bae8c6fe6d13a3351517a
XiomaraS <scarlita_xiomara@hotmail.com>
2017-11-12 17:37:31 -0600

    Add files via upload

---
+++ Proveedores1/Proveedores1/TranProveedores.cs
@@ -0,0 +1,64 @@
+﻿/*Raul Portillo 
+  0901-13-14547
+ */
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Odbc;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Proveedores1
+{
+    public partial class TranProveedores : Form
+    {
+        public int idApp = 6203;
+        public TranProveedores()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MantTranProveedores mov = new MantTranProveedores();
+            mov.Show();
+            this.Close();
+        }
+
+        private void TranProveedores_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                string MyConnection2 = "Driver ={ MySQL ODBC 3.51 Driver}; Dsn=bd_hotel; UID=root; PWD=;";
+                // string MyConnection2 = "server=localhost;user id=root;database=hotelsancarlosv2";
+                string Query = "select * from transacciones_proveedores;";
+                OdbcConnection MyConn2 = new OdbcConnection(MyConnection2);
+                OdbcCommand MyCommand2 = new OdbcCommand(Query, MyConn2);
+
+                OdbcDataAdapter MyAdapter = new OdbcDataAdapter();
+                MyAdapter.SelectCommand = MyCommand2;
+                DataTable dTable = new DataTable();
+                MyAdapter.Fill(dTable);
+                Dgv_TranProv.DataSource = dTable;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        private void Dgv_TranProv_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            MantTranProveedores mov = new MantTranProveedores();
+            mov.id = Dgv_TranProv.CurrentRow.Cells[0].Value.ToString();
+            mov.Txt_decrip.Text = Dgv_TranProv.CurrentRow.Cells[1].Value.ToString();
+            mov.Txt_cod.Text = Dgv_TranProv.CurrentRow.Cells[0].Value.ToString();
+            mov.Txt_decrip.Enabled = false;
+            mov.Txt_cod.Enabled = false;
+            mov.Show();
+        }
+    }
+}
